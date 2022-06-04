755e14821865f4cc36ce15db9d3056e801284e57
emagdnimMQT <mark.w.novak@gmail.com>
2022-06-04 14:48:09 -0400

    Trying to get dotenv to work.

---
+++ main.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+console.log(process.env)
+const key = process.env.YOUTUBE;
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API%20&type=video&videoCaption=closedCaption&key=${key}`;
+console.log(url)
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", url, true);
+xhttp.send();
