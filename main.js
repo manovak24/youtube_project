1d7812c2b11451661a7cced4f10f47580c2df31e
emagdnimMQT <mark.w.novak@gmail.com>
2022-06-08 16:04:44 -0400

    Got the API to work to display the string data.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const youtubeKey = config.YOUTUBE_API;
 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API%20&type=video&videoCaption=closedCaption&key=${youtubeKey}`;
-console.log(url)
 const xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       document.getElementById("demo").innerHTML = xhttp.responseText;
+    // document.getElementById("demo").innerHTML = xhttp.responseText;
+    // console.log(xhttp.responseText);
+    // console.log(typeof xhttp.responseText);
+
+    const dataSet = JSON.parse(xhttp.responseText)
+    console.log(dataSet);
     }
 };
 xhttp.open("GET", url, true);
