0cb6298db3e0f3cd508186ffa42626025a6f3100
emagdnimMQT <mark.w.novak@gmail.com>
2022-06-13 11:59:10 -0400

    Got API working and will refractor for web app.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchTerm = document.getElementById('search-term');
 const submitBtn = document.getElementById('submit-btn');
 const div = document.getElementById('demo');
+const link = document.getElementById('link');
 const youtubeKey = config.YOUTUBE_API;
 let youtubeSeachTerm = '';
 
@@ -11,18 +12,17 @@ submitBtn.addEventListener('click', (e) => {
     
     // YouTube+Data+API%20 most likely will be turned into the search term but will need to look into this more
         const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeSeachTerm}&key=${youtubeKey}`;
+        console.log(url)
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            // document.getElementById("demo").innerHTML = xhttp.responseText;
-            console.log(xhttp.responseText);
-            // console.log(typeof xhttp.responseText);
+            // console.log(xhttp.responseText);
 
-            // const dataSet = JSON.parse(xhttp.responseText)
-            // console.log(dataSet)
-            // const test = `https://www.youtube.com/watch?v=${dataSet.items[0].id.videoId}`;
-            // console.log(test)
+            const dataSet = JSON.parse(xhttp.responseText)
+            console.log(dataSet)
+            const test = `https://www.youtube.com/watch?v=${dataSet.items[0].id.videoId}`;
+            link.href = test;
+            link.innerText = 'Hello World';
 
             // const div = document.getElementById('demo');
             // const link = document.createElement('a');
