e10d8682a3349e9fc262120dddfcb3c315be1fa1
emagdnimMQT <mark.w.novak@gmail.com>
2022-06-30 08:37:09 -0400

    Refractored script to have event listener for click and enter keypress.

--- main.js
+++ main.js
@@ -33,55 +33,62 @@ const timeAgo = (date) => {
     return `${interval} ${epoch}${suffix} ago`;
 };
 
-submitBtn.addEventListener('click', function (e) {
+const eventHandler = (e) => {
     youtubeSeachTerm = searchTerm.value;
     e.preventDefault();
     // console.log(youtubeSeachTerm)
     
     // YouTube+Data+API%20 most likely will be turned into the search term but will need to look into this more
-        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&order=rating&q=${youtubeSeachTerm}&maxResults=10&key=${youtubeKey}`;
-        // console.log(url)
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            // console.log(xhttp.responseText);
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&order=rating&q=${youtubeSeachTerm}&maxResults=10&key=${youtubeKey}`;
+    // console.log(url)
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        // console.log(xhttp.responseText);
 
-                const dataSet = JSON.parse(xhttp.responseText)
-                const data = dataSet.items;
-                // console.log(data)
+            const dataSet = JSON.parse(xhttp.responseText)
+            const data = dataSet.items;
+            // console.log(data)
 
-                for(let item in data) {
-                    // const videoLink = document.createElement('a');
-                    // videoLink.href = `https://www.youtube.com/watch?v=${dataSet.items[item].id.videoId}`;
-                    // videoLink.innerText = `${dataSet.items[item].snippet.title}`;
-                    // videoLink.target = '_blank';
-                    // searchResults.appendChild(videoLink);
+            for(let item in data) {
+                // const videoLink = document.createElement('a');
+                // videoLink.href = `https://www.youtube.com/watch?v=${dataSet.items[item].id.videoId}`;
+                // videoLink.innerText = `${dataSet.items[item].snippet.title}`;
+                // videoLink.target = '_blank';
+                // searchResults.appendChild(videoLink);
 
-                    console.log(data[item]);
+                console.log(data[item]);
 
-                    const videoDiv = document.createElement('div');
-                    videoDiv.classList.add('result-ctr')
-                    videoDiv.innerHTML = `
-                        <a href="https://www.youtube.com/watch?v=${dataSet.items[item].id.videoId}" target="_blank">
-                            <img src="${dataSet.items[item].snippet.thumbnails.medium.url}" class="video-thumbnail">
-                        </a>
-                        <a href="https://www.youtube.com/watch?v=${dataSet.items[item].id.videoId}" target="_blank">
-                            <h4>${dataSet.items[item].snippet.title}</h4>
-                        </a>
-                        <a href="https://www.youtube.com/c/${dataSet.items[item].snippet.channelTitle}" target="_blank">
-                            <h5>${dataSet.items[item].snippet.channelTitle}</h5>
-                        </a>
-                        <p class="publish-time">${timeAgo(dataSet.items[item].snippet.publishTime)}</p>
-                        
-                    `
-                    searchResults.appendChild(videoDiv);
-                }
-    
-
+                const videoDiv = document.createElement('div');
+                videoDiv.classList.add('result-ctr')
+                videoDiv.innerHTML = `
+                    <a href="https://www.youtube.com/watch?v=${dataSet.items[item].id.videoId}" target="_blank">
+                        <img src="${dataSet.items[item].snippet.thumbnails.medium.url}" class="video-thumbnail">
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=${dataSet.items[item].id.videoId}" target="_blank">
+                        <h4>${dataSet.items[item].snippet.title}</h4>
+                    </a>
+                    <a href="https://www.youtube.com/c/${dataSet.items[item].snippet.channelTitle}" target="_blank">
+                        <h5>${dataSet.items[item].snippet.channelTitle}</h5>
+                    </a>
+                    <p class="publish-time">${timeAgo(dataSet.items[item].snippet.publishTime)}</p>
+                    
+                `
+                searchResults.appendChild(videoDiv);
             }
-        };
-        xhttp.open("GET", url, true);
-        xhttp.send();
+
+
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+submitBtn.addEventListener('click', eventHandler);
+submitBtn.addEventListener('click', function(e) {
+    if(e.key === 'Enter') {
+        eventHandler();
+    }
 });
 
 
